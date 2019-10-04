bb6bef6c4e486e61f4688c540e4820a3cf73364d
Barun Sarraf <barun.sarraf@cgi.com>
2019-10-04 12:40:37 +0530

    created movie restful api

---
+++ src/main/java/com/stackroute/Movie/Initiliazer/CommandLineRunner.java
@@ -0,0 +1,30 @@
+package com.stackroute.Movie.Initiliazer;
+
+import com.stackroute.Movie.Exception.MovieNotFoundException;
+import com.stackroute.Movie.model.Movie;
+import com.stackroute.Movie.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+public class CommandLineRunner implements org.springframework.boot.CommandLineRunner {
+    MovieService movieService;
+
+    @Autowired
+    public CommandLineRunner(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("this is via command line runner");
+        try {
+            Movie movie= new Movie(6,"CommandLineRunner Mission Impossible","Tom Cruise","Action",new BigDecimal(1000),"Wednesday 2018");
+            movieService.saveMovie(movie);
+        } catch (MovieNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
